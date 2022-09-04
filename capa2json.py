deba4c2e0ecf9aa1276f9c8219487663f7091ba2
stav <shpstv@gmail.com>
2022-09-04 12:31:41 +0300

    capa2json script works

--- capa2json.py
+++ capa2json.py
@@ -2,13 +2,18 @@
 Description:
     A script to convert an output file of CAPA to a json file
 """
+from dataclasses import dataclass
+import dataclasses
 import json
 import os
+from pathlib import Path
 from sys import argv
 import re
+from typing import List
+from tqdm import tqdm
 
 CAPABILITY_NAMESPACE = "CAPABILITY"
-MATCHES_PATTERN = r"\((\d+) matches\)"
+MATCHES_PATTERN = "\((\d+) matches\)"
 FILE_ID = "sha256"
 CAPAS_KEY_NAME = "capas"
 
@@ -18,33 +23,57 @@ OUT_FILE_SUFFIX = "json"
 def show_usage():
     print(f"Usage: ./{os.path.basename(__file__)} [input_file] [output_file]")
 
+@dataclass
+class CapaRule:
+    rule:str
+    matches:int
+    namespace:str
+    
+    def to_dict(self):
+        return dataclasses.asdict(self)
 
-def tuple_dict_get(tuple_dict, key):
+# class CapaRule:
+#     def __init__(self, rule, matches, namespace) -> None:
+#         self.rule = rule
+#         self.matches = matches
+#         self.namespace = namespace
+    
+#     def to_dict(self) -> dict:
+#         """
+#         Converts the CapaRule into a dictionary
+#         """
+#         return {
+#             "rule" : self.rule,
+#             "matches" : self.matches,
+#             "namespace" : self.namespace
+#         }
+
+def get_rule_by_key(capa_rules: List[CapaRule], key: str):
     """
-    Get the value of a key from a tuple dictionary
-    :param tuple_dict: the dictionary of tuples to retrieve the value from
+    Get the value of a key from a list 
+    :param capa_rules: the capa rules list
     :param key: the key to retrieve the value of
     :return: the value of the key
     """
-    for item in tuple_dict:
-        if len(item) == 2 and item[0] == key:
-            return item[1]
+    for capa_rule in capa_rules:
+        if capa_rule.rule == key:
+            return capa_rule.namespace
 
 
-def get_capabilities(capa_list):
+def get_capabilities(capa_list: List[CapaRule]):
     """
     Get the capabilities from a list of an unparsed capa lines
     :param capa_list: the list of capas
     :return: parsed capabilities lists
     """
-    return capa_list[([item[0] for item in capa_list].index(""))+1:]
+    index = 0 
+    for i, capa_rule in enumerate(capa_list):
+        if capa_rule.rule == CAPABILITY_NAMESPACE:
+            index = i
+            break
+    return capa_list[index+1:]
 
-
-def main(input_file, output_file):
-    if not os.path.exists(input_file):
-        print(f"File {input_file} does not exist")
-        return
-
+def capa_to_json(input_file, output_file):
     with open(input_file, "r") as capa_file:
         capa_content = capa_file.readlines()
 
@@ -52,29 +81,47 @@ def main(input_file, output_file):
     for line in capa_content:
         line = line.split("|")
         if len(line) == 4:
-            key, value = line[1].strip(), line[2].strip()
+            rule, namespace = line[1].strip(), line[2].strip()
 
-            if re.match(MATCHES_PATTERN, key):
-                matches = re.findall(MATCHES_PATTERN, key)[0]
-                key = key[:key.rfind("(")].strip()
+            if matches:=re.findall(MATCHES_PATTERN, rule):
+                matches = int(matches[0])
+                rule = rule[:rule.rfind("(")].strip()
             else:
                 matches = 1
 
-            lines_dict.append((key, matches, value))
+            lines_dict.append(CapaRule(rule, matches, namespace))
 
-    file_id = tuple_dict_get(lines_dict, FILE_ID)
+    file_id = get_rule_by_key(lines_dict, FILE_ID)
     capas = get_capabilities(lines_dict)
 
-    capas_json = {FILE_ID: file_id, CAPAS_KEY_NAME: capas}
+    capas_json = {FILE_ID: file_id, CAPAS_KEY_NAME: [capa_rule.to_dict() for capa_rule in capas]}
 
     with open(f"{output_file}.{OUT_FILE_SUFFIX}", "w") as output_file:
         json.dump(capas_json, output_file, indent=4)
+
+
+def main(input_file, output_file):
+    if not os.path.exists(input_file):
+        print(f"File {input_file} does not exist")
+        return
+    
+    if os.path.isdir(input_file):
+        p=Path(input_file)
+        for txt_file in tqdm(p.rglob("*.txt")):
+            current_output_file = (Path(output_file)/txt_file.relative_to(p))
+            current_output_file.parent.mkdir(parents=True, exist_ok=True)
+            current_output_file = str(current_output_file).replace('.txt','.json')
+            capa_to_json(txt_file, current_output_file)
+    else:
+        capa_to_json(input_file, output_file)
+    
+   
 
 
 if __name__ == "__main__":
     if len(argv[1:]) == 2:
         main(argv[1], argv[2])
     elif len(argv[1:]) == 1:
-        main(argv[1], os.path.basename(argv[0]))
+            main(argv[1], os.path.abspath(argv[0]))
     else:
         show_usage()
